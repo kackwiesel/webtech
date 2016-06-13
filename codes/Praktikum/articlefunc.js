5c9c9ae9e4ee6b55a1b8bdb4d5e2a3a8b0699ffd
kackwiesel <kkscheler@gmx.de>
2016-06-13 17:57:14 +0200

    hamburger menu added

--- codes/Praktikum/articlefunc.js
+++ codes/Praktikum/articlefunc.js
@@ -1,26 +1,51 @@
 function change_article(id){
 
-// bei einer Aktion zunächst alle ausblenden
-AlleAus()
+//get all articles
+var articles=document.getElementsByTagName("article");
 
-var article = document.getElementById(id);
-
-// Prüfung exixtiert eine Element mit dieser ID?
-if(!article)return true;
-
-  // Umschaltung
-  if(article.style.display == "none")
-  {
-  article.style.display = "block"
-  }
-  else
-  {
-  article.style.display = "none"
-  }
+//iterate over articles, set display to block if ID matches desired article
+for (i=0; i<articles.length;i++){
+	if (articles[i].id==id){
+		articles[i].style.display = "block";
+    	}else{
+		articles[i].style.display = "none";
+	}
+}
 
 return true;
 }
-function AlleAus() {
-   about_me.style.display = "none";
-   kompetenzen.style.display = "none";
+
+function hamburger(){
+  var icon = document.getElementById('hamburger');
+  icon.style.display = "none";
+
+  var list = document.getElementById('list');
+  list.style.display = "block";
 }
+
+// function change_article(id){
+//
+// // bei einer Aktion zunächst alle ausblenden
+// AlleAus()
+//
+// var article = document.getElementById(id);
+//
+// // Prüfung exixtiert eine Element mit dieser ID?
+// if(!article)return true;
+//
+//   // Umschaltung
+//   if(article.style.display == "none")
+//   {
+//   article.style.display = "block"
+//   }
+//   else
+//   {
+//   article.style.display = "none"
+//   }
+//
+// return true;
+// }
+// function AlleAus() {
+//    about_me.style.display = "none";
+//    kompetenzen.style.display = "none";
+// }
---
+++ codes/Praktikum/hamburger.js
@@ -0,0 +1,7 @@
+function hamburger(){
+  var icon=getElementById('hamburger');
+  icon.style.display = "none";
+
+  var list=getElementById('list');
+  list.style.display = "block";
+}
